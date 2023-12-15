7e8444a6b48cce59e6839fbf0bd5429142c75dd3
Fritz Leo Ochsmann <53190645+Randoooom@users.noreply.github.com>
2023-12-15 16:13:55 +0100

    refactor: make link const

--- lib/navigation.dart
+++ lib/navigation.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
+const String homepage = "https://tu-dresden.de";
+
 class UniAppBar extends StatelessWidget implements PreferredSizeWidget {
   // impl PreferredSizeWidget
   @override
@@ -16,8 +18,9 @@ class UniAppBar extends StatelessWidget implements PreferredSizeWidget {
       title: const Text('Mein UNI-Tag'),
       centerTitle: true,
       leading: IconButton(
-        //navigate to the tud-homepage
-        onPressed:  () async => await launchUrlString("https://tu-dresden.de"),
+        // open the homepage in the browser (yea we ignore the possibility of an error while launching the browser)
+        // TODO: consider this as a home button for the application in the case we add multiple views / pages
+        onPressed:  () async => await launchUrlString(homepage),
         icon: const Icon(Icons.foundation),
       ),
       actions: [
