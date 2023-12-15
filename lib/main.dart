f6f132685b081ac0515544cffd8960f6e02a91d1
Fritz Leo Ochsmann <53190645+Randoooom@users.noreply.github.com>
2023-12-15 15:38:07 +0100

    refactor: move appBar into own class

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,45 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const UniApp());
 
-class MyApp extends StatelessWidget {
+class UniApp extends StatelessWidget {
+  const UniApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Mein UNI-Tag'),
-          leading: IconButton(
-            onPressed: (
-                //Go to TU Dresden homepage
-                ) {},
-            icon: Icon(Icons.home),
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.menu),
-            ),
-          ],
-        ),
+      home: const Scaffold(
+        appBar: UniAppBar(),
+      ),);
+  }
+}
+
+class UniAppBar extends StatelessWidget implements PreferredSizeWidget {
+  // impl PreferredSizeWidget
+  @override
+  final Size preferredSize;
+
+  const UniAppBar({Key? key})
+      : preferredSize = const Size.fromHeight(kToolbarHeight),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text('Mein UNI-Tag'),
+      leading: IconButton(
+        onPressed: (//Go to TU Dresden homepage
+            ) {},
+        icon: Icon(Icons.home),
       ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
+        ),
+      ],
     );
   }
 }
